f0b5c129f48d44a01a7cbc7bbae1f49930e2b81e
Kone Balachandra <84697688+bala9908@users.noreply.github.com>
2025-08-04 22:18:31 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 import Skills from "./pages/Skills";
 import { useNavigate } from 'react-router-dom';
 const HeroSection = () => {
-  let naviage=useNavigate()
+  let naviagate=useNavigate()
   const socialLinks = [
     { icon: Github, href: 'https://github.com/bala9908', label: 'GitHub' },
     { icon: Linkedin, href: 'https://www.linkedin.com/in/kone-balachandra-510390213', label: 'LinkedIn' },
@@ -49,7 +49,7 @@ const HeroSection = () => {
               <Button 
                 variant="outline" 
                 className="border-primary text-primary hover:bg-primary/10 hover:border-primary/50 transition-all duration-300"
-                onClick=()=>{navigae('/skills')}
+                onClick={()=>navigate('/skills')}
               >
                 <ExternalLink className="w-5 h-5 mr-2" />
                 View Projects
