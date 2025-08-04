632b0156d7314bf9f4ba85f9a5493dcad48a13b1
Kone Balachandra <84697688+bala9908@users.noreply.github.com>
2025-08-04 22:21:16 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,6 +2,7 @@ import { Github, Linkedin, Mail, Download, ExternalLink } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import Skills from "./pages/Skills";
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const HeroSection = () => {
   let naviagate=useNavigate()
   const socialLinks = [
@@ -46,14 +47,16 @@ const HeroSection = () => {
                 Download Resume
               </Button>
                 </a>
+              <Link to="/skills" >
               <Button 
                 variant="outline" 
                 className="border-primary text-primary hover:bg-primary/10 hover:border-primary/50 transition-all duration-300"
-                onClick={()=>navigate('/skills')}
+                 
               >
                 <ExternalLink className="w-5 h-5 mr-2" />
                 View Projects
               </Button>
+                </Link>
             </div>
 
             {/* Social Links */}
